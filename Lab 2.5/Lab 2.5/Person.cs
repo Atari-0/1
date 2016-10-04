5ccd45f715fc23944843c6e18d691fb57f3f5839
Atari-0 <sewell.amari@yahoo.com>
2016-10-04 19:13:50 -0400

    Add files via upload

---
+++ Lab 2.5/Lab 2.5/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2._5
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+        public static double SumOfAllAges;
+        double AverageOfAllAges;
+        
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.Write(GetFullName() + ",Age:" + Age);
+        }
+
+
+        public void UserInformation()
+        {
+            System.Console.Write("What is your first name? : ");
+            FirstName = (System.Console.ReadLine());
+            System.Console.Write("What is your last name? : ");
+            LastName = (System.Console.ReadLine());
+            System.Console.Write("Your full name is: " + GetFullName() + ". Please Press the Enter Key to Continue");
+            System.Console.ReadLine();
+            System.Console.Write("What is your Age?: ");
+            Age = int.Parse(System.Console.ReadLine());
+            Person.SumOfAllAges += Age;
+            this.Spouse = new Person();
+            System.Console.Write("What is your spouse's first name? : ");
+            Spouse.FirstName = (System.Console.ReadLine());
+            Spouse.LastName = LastName;
+            System.Console.Write("Your Spouse's full name is : " + Spouse.GetFullName() + ". Please Press the Enter Key to Continue");
+            System.Console.ReadLine();
+            System.Console.Write("How old is your spouse?: ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+            Person.SumOfAllAges += Spouse.Age;
+
+            System.Console.Write("The sum of both couple's ages is : " + SumOfAllAges);
+            AverageOfAllAges = SumOfAllAges / 4;
+            System.Console.Write(". The average of all the ages is : " + AverageOfAllAges);
+            System.Console.ReadLine(); 
+
+            
+
+
+
+        }
+
+    }
+}
---
+++ Lab 2.5/Lab 2.5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+
+            p1.UserInformation();
+
+
+            System.Console.WriteLine("Summary of User's answers");
+            p1.PrintNameAndAge();
+            System.Console.ReadLine();
+            System.Console.WriteLine("Summary of Spouse's Answers");
+            p1.Spouse.PrintNameAndAge();
+            System.Console.ReadLine();
+
+            System.Console.Write("Now pass the console to the next user. Please Press Enter to continue afterwards.");
+            System.Console.ReadLine();
+            Person p2 = new Person();
+
+            p2.UserInformation();
+            System.Console.WriteLine("Summary of User's answers");
+            p2.PrintNameAndAge();
+            System.Console.ReadLine();
+            System.Console.WriteLine("Summary of Spouse's answers");
+            p2.Spouse.PrintNameAndAge();
+            System.Console.ReadLine(); 
+
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
